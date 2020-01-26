95d99e9c4c3e37a1c9719a5ef0e0a841e011913f
0siris7 <sarang.prakash2@gmail.com>
2020-01-26 12:39:58 +0530

    fixed css in adminhome, userhome

--- Project/main.py
+++ Project/main.py
@@ -2,7 +2,6 @@ from flask import Flask
 from public import public
 from admin import admin
 from user import user
-# from register import register
 app=Flask(__name__)
 app.register_blueprint(public)
 app.register_blueprint(admin,url_prefix="/admin")
